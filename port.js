23847ed8494e5425400a0da689221a2d39bf3497
DevUnanam <riaahh20@yahoo.com>
2024-04-30 05:27:35 -0700

    first commit for my portfolio file

---
+++ port.js
@@ -0,0 +1,23 @@
+
+/*this section Automatically collapse the navbar menu after clicking a link on mobile devices.*/
+$(document).ready(function() {
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+});
+
+
+/*this section Add smooth scrolling to your navigation links so that 
+when users click on a link, the page scrolls smoothly to the corresponding section*/
+$(document).ready(function(){
+    // Smooth scrolling for links
+    $('a[href^="#"]').on('click', function(event) {
+        var target = document.querySelector(this.getAttribute('href'));
+        if( target ) {
+            event.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+
